6571a326aef57b5e6c2bc8d94e0be335222e02e3
“Ryan” <pagsmanagement@gmail.com>
2022-10-23 23:03:36 -0500

    initiail commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import InputCard from './components/InputCard';
+import TitleCard from './components/TitleCard';
 
 function App() {
+  const [next, setNext] = useState<number>(0)
+  const [gender, setGender] = useState<number>(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TitleCard />
+      <InputCard setNext={setNext} next={next} gender={gender} setGender={setGender} />
     </div>
   );
 }
---
+++ src/components/TitleCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Styles.css'
+
+function TitleCard() {
+    return (
+        <span className='Title'>
+            Sports Submit
+        </span>
+    )
+}
+
+export default TitleCard
